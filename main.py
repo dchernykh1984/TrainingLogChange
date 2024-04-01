75b610f1061db4cd7eed8668ba98b0505024543e
Denis Chernykh <d_chernykh@inbox.ru>
2024-04-01 22:00:19 +0500

    add argparse instead of hardcoded values

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
+import argparse
+from datetime import datetime
+
 from tcx_modifier import TCXModifier
 
 
+def valid_date(s):
+    try:
+        datetime.strptime(s, "%Y-%m-%dT%H:%M:%S.%fZ")
+        return s
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+
 if __name__ == "__main__":
-    modifier = TCXModifier("data.tcx")
-    modifier.cleanup_power(2000.0)
-    modifier.cleanup_canence(200)
-    modifier.cleanup_heart_rate(200)
-    modifier.speedup(1.8)
-    modifier.update_start_time("2024-03-31T15:53:51.000Z")
-    modifier.print_field("Lap")
-    modifier.save("modified_data.tcx")
+    # Function to validate and parse datetime string
+
+    # Create the parser
+    parser = argparse.ArgumentParser(description='Process input and output file paths, and other parameters.')
+
+    # Add the arguments
+    parser.add_argument('input', type=str, help='Input file path')
+    parser.add_argument('output', type=str, help='Output file path')
+    parser.add_argument('--speedup', '-s', type=float, required=False, help='Speedup percentage (float)')
+    parser.add_argument('--max_hr', type=int, required=False, help='Maximal heart rate (integer)')
+    parser.add_argument('--max_power', type=float, required=False, help='Maximal power (float)')
+    parser.add_argument('--max_cadence', type=int, required=False, help='Maximal cadence (integer)')
+    parser.add_argument('--start_date', type=valid_date, required=False,
+                        help='Start date time in format "%Y-%m-%dT%H:%M:%S.%fZ"')
+    args = parser.parse_args()
+
+    print(f"{args.speedup=}")
+    print(f"{args.max_hr=}")
+    print(f"{args.max_power=}")
+    print(f"{args.max_cadence=}")
+    print(f"{args.start_date=}")
+
+    modifier = TCXModifier(args.input)
+    if args.max_power:
+        modifier.cleanup_power(args.max_power)
+    if args.max_cadence:
+        modifier.cleanup_canence(args.max_cadence)
+    if args.max_hr:
+        modifier.cleanup_heart_rate(args.max_hr)
+    if args.speedup:
+        modifier.speedup(args.speedup)
+    if args.start_date:
+        modifier.update_start_time(args.start_date)
+    modifier.save(args.output)
